7d745fd2bdd2d45638cb08b013edc828311fc496
hayden7913 <hayden321@gmail.com>
2020-07-24 14:44:55 -0700

    configures elephantsql database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://localhost:5432/hayden'
+  DB_URL: process.env.DB_URL || 'postgres://juecbrnl:tqPAgcx-60LiCOUya4MbvMTJBZbaYjE5@ruby.db.elephantsql.com:5432/juecbrnl'
 }
